90a16f40fc889d1798dff57d7f05719f0471bdb2
Amanditas <allonsmandy@gmail.com>
2019-08-19 23:06:30 -0300

    nem acredito que deu certo

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
+'use strict';
+
 var gulp = require('gulp'); //carrega o modulo instalado e armazena nesta variavel
 var sass = require('gulp-sass');
+var uglifycss = require('gulp-uglifycss');
 
 //tarefinha
 gulp.task('sass', function() {
-    gulp.src('./src/sass/**/*.scss') //pega os arquivos de origem 
-        .pipe(sass()) //faz um tratamento utilizando o gulp-sass
-        .pipe(gulp.dest('./src/css/')); //manda pra saida
+    return gulp.src('./src/sass/**/*.scss') //pega os arquivos de origem 
+        .pipe(sass().on('error', sass.logError)) //faz um tratamento utilizando o gulp-sass
+        .pipe(gulp.dest('./src/css')); //manda pra saida
+})
+
+gulp.task('css', function() {
+    gulp.src('./src/css/*.css')
+        .pipe(uglifycss({
+            "uglyComments": true
+        }))
+        .pipe(gulp.dest('./dist/'));
 })
 
 //monitorando arquivos
 gulp.task('listen', function() {
-    gulp.watch('./src/sass/**/*.scss', ['sass']); //arquivos que serao monitorados e se for executado uma alteração ele roda a tarefa anterior
+    gulp.watch('./src/sass/**/*.scss', gulp.series('sass')); //arquivos que serao monitorados e se for executado uma alteração ele roda a tarefa anterior
+    gulp.watch('./src/css/*.css', gulp.series('css'))
 });
